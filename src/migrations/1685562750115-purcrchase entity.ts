bf69586e9b49353b91d0f49fa0d40a1faaf11196
HardBoris <bgaibor03@gmail.com>
2023-05-31 16:23:44 -0400

    feat: purchase migration

---
+++ src/migrations/1685562750115-purcrchase entity.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class PurcrchaseEntity1685562750115 implements MigrationInterface {
+    name = 'PurcrchaseEntity1685562750115'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."purchases_logisticmode_enum" AS ENUM('Entrega', 'Retirada')`);
+        await queryRunner.query(`CREATE TYPE "public"."purchases_paymentform_enum" AS ENUM('Faturado', 'Cartão', 'A vista')`);
+        await queryRunner.query(`CREATE TYPE "public"."purchases_paymentinstallments_enum" AS ENUM('1X', '2X', '3X', 'Pix', 'Dinheiro')`);
+        await queryRunner.query(`CREATE TYPE "public"."purchases_purchasestatus_enum" AS ENUM('Ok', 'Pendente', 'Atrasada')`);
+        await queryRunner.query(`CREATE TABLE "purchases" ("purchaseId" uuid NOT NULL DEFAULT uuid_generate_v4(), "purchaseDate" TIMESTAMP NOT NULL DEFAULT now(), "purchaseReference" character varying, "deliveryDate" TIMESTAMP NOT NULL, "logisticMode" "public"."purchases_logisticmode_enum" NOT NULL DEFAULT 'Entrega', "paymentForm" "public"."purchases_paymentform_enum" NOT NULL DEFAULT 'Faturado', "paymentInstallments" "public"."purchases_paymentinstallments_enum" NOT NULL DEFAULT '3X', "purchaseStatus" "public"."purchases_purchasestatus_enum" NOT NULL DEFAULT 'Pendente', "supplierId" uuid, CONSTRAINT "PK_611866f7af176a877f97cbb76a4" PRIMARY KEY ("purchaseId"))`);
+        await queryRunner.query(`ALTER TABLE "purchases" ADD CONSTRAINT "FK_77980c752fdeb3689e318fde424" FOREIGN KEY ("supplierId") REFERENCES "suppliers"("supplierId") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "purchases" DROP CONSTRAINT "FK_77980c752fdeb3689e318fde424"`);
+        await queryRunner.query(`DROP TABLE "purchases"`);
+        await queryRunner.query(`DROP TYPE "public"."purchases_purchasestatus_enum"`);
+        await queryRunner.query(`DROP TYPE "public"."purchases_paymentinstallments_enum"`);
+        await queryRunner.query(`DROP TYPE "public"."purchases_paymentform_enum"`);
+        await queryRunner.query(`DROP TYPE "public"."purchases_logisticmode_enum"`);
+    }
+
+}
